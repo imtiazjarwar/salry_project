036d22e27a6a20600ab062e74a13e9715b35a76c
imtiaz Hussain Jarwar <101209687+imtiazjarwar@users.noreply.github.com>
2023-06-25 23:19:25 +0500

    Add files via upload

---
+++ Salry Program/js/script.js
@@ -0,0 +1,9 @@
+let Basic=prompt("Enter Your Basic Salry","");
+let Per=prompt("Enter Your Persantage %","");
+let cont=(Per/100);
+let Mult=(Basic*cont);
+
+
+document.write(`The Value Of Basic Pay ${Basic}`,"<br>");
+document.write(`The Increse ${Per}%`,"<br>");
+document.write(`Total Incarse Salry`,"<br>");
